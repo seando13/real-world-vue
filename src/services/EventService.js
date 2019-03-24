f8ee62bb9ad0742349db4ada6217a3c026a70622
Sean McCusker <sean.mccusker@gmail.com>
2019-03-24 22:43:21 +1100

    Changed source for axios call

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://git.heroku.com/real-world-vue1.git',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
