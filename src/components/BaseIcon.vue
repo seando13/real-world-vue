4871b12d7ce8451d2b01d85306de02b9f9a0015f
Sean McCusker <sean.mccusker@gmail.com>
2019-03-25 20:12:20 +1100

    Setting State and Getters in Vue

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="icon-wrapper">
-    <svg class="icon" :width="width" :height="height">
-      <use v-bind="{'xlink:href':'/feather-sprite.svg#'+name}"></use>
-    </svg>
-    <slot></slot>
-  </div>
+    <div class="icon-wrapper">
+      <svg class='icon' :width="width" :height="height">
+        <use v-bind="{'xlink:href':'/feather-sprite.svg#' + name}"/>
+      </svg>
+      <slot></slot>
+    </div>
 </template>
 
 <script>
 export default {
+  name: 'Icon',
   props: {
     name: String,
     width: {
--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
-  withCredentials: false,
+  baseURL: `http://localhost:3000`,
+  withCredentials: false, // This is the default
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
